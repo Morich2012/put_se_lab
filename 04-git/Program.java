04d611fc4252bdda233a4a91fc3fdad36a2bdf45
Miguel Morales <morich2012real@gmail.com>
2026-04-10 22:30:47 +0200

    Change 1

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -1,6 +1,18 @@
 public class Program {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
         System.out.println("New Feature");
+
+        System.out.println("Change 1");   // cambio 1
+
+
+        System.out.println("New Feature");
+
+        System.out.println("Change 2");   // cambio 2
+
+        System.out.println("Change 3");   // cambio 3
     }
 }
+
+
